ddf212b2c8edb802c3bfb4cc98f77bfc15cc3975
Rahul Hande <rahulsandiphande@gmail.com>
2023-09-28 15:17:55 +0530

    Script2 is modified.

--- Script2.sh
+++ Script2.sh
@@ -2,7 +2,7 @@
 #This script is to read input from the user/console
 echo "Value of a"
 read a
-echo "Value of b"
-read b
-echo "Hello value of a is $a and value of b is $b"
+echo "Value of c"
+read c
+echo "Hello value of a is $a and value of c is $c"
 echo "We are performing this practical for batch-20"
